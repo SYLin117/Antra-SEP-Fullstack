d03eb870410a819f37f072cc9414b89d2ca2b1c7
SYLin117 <newianlin@gmail.com>
2023-02-14 16:30:04 -0800

    SQL day1 assignment

---
+++ SQL/SQL_Day1_assignment.sql
@@ -0,0 +1,76 @@
+--Q1
+select ProductID, [Name], Color, ListPrice
+from Production.product
+
+--Q2
+select ProductID, [Name], Color, ListPrice
+from Production.product
+where ListPrice = 0
+
+--Q3
+select ProductID, [Name], Color, ListPrice
+from Production.product
+where Color is NULL
+
+--Q4
+select ProductID, [Name], Color, ListPrice
+from Production.product
+where Color is not NULL
+
+--Q5
+select ProductID, [Name], Color, ListPrice
+from Production.product
+where (Color is not Null and ListPrice > 0)
+
+--Q6
+select Color + ' ' + [Name]
+from Production.product
+where Color is not NULL
+
+--Q7
+select 'NAME: '+[Name]+ ' -- '+ color
+from Production.Product
+where color is not NULL
+
+--Q8
+select ProductID, [Name]
+from Production.product
+where ProductId BETWEEN 400 and 500
+
+--Q9
+select ProductID, [Name], Color
+from Production.Product
+where Color IN ('black', 'blue')
+
+--Q10
+select [Name]
+from Production.Product
+where [Name] like 'S%'
+
+--Q11
+select [Name], ListPrice
+from Production.Product
+where [Name] LIKE 'S%'
+order by [Name]
+
+--Q12
+select [Name], ListPrice
+from Production.Product
+order by [Name]
+
+--Q13
+select [Name]
+from Production.Product
+where [Name] LIKE '[SPO][^K]%'
+order by [Name]
+
+--Q14
+select Distinct Color
+from Production.Product
+order by Color DESC
+
+--Q15
+select DISTINCT CAST(pp.ProductSubcategoryID as varchar) + ' ' + pp.Color as Result
+from Production.Product pp
+where (pp.ProductSubcategoryID is not NULL and pp.Color is not NULL)
+order by Result
